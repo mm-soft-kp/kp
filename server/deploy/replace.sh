5d020963b23f4c508bba565d13e221197fb93b32
z <z>
2020-11-23 14:19:26 +0800

    init

--- server/deploy/replace.sh
+++ server/deploy/replace.sh
@@ -1,10 +1,10 @@
 #replace.sh 用于将上次构建的结果备份，然后将新的构建结果移动到合适的位置
 #!/bin/bash
 # 先判断文件是否存在，如果存在，则备份
-file="/data/www/server/kp/kp.server-1.0-SNAPSHOT.jar"
+file="/data/www/kp/server/kp.server-1.0-SNAPSHOT.jar"
 if [ -f "$file" ]
 then
-   mv /data/www/server/kp/kp.server-1.0-SNAPSHOT.jar.`date +%Y%m%d%H%M%S`
+   mv /data/www/kp/server/kp.server-1.0-SNAPSHOT.jar.`date +%Y%m%d%H%M%S`
 fi
-mv /root/.jenkins/workspace/kp/server/target/kp.server-1.0-SNAPSHOT.jar /data/www/server/kp/kp.server-1.0-SNAPSHOT.jar
+mv /root/.jenkins/workspace/kp/server/target/kp.server-1.0-SNAPSHOT.jar /data/www/kp/server/kp.server-1.0-SNAPSHOT.jar
 
