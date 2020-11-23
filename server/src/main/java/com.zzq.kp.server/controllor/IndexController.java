0d9035b8a4f22775d91f5f5570d8dd9fd13c385a
z <z>
2020-11-23 15:59:12 +0800

    init

--- server/src/main/java/com.zzq.kp.server/controllor/IndexController.java
+++ server/src/main/java/com.zzq.kp.server/controllor/IndexController.java
@@ -1,5 +1,6 @@
 package com.zzq.kp.server.controllor;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "index")
+
 public class IndexController {
     @RequestMapping(value = "hello")
+    @ApiOperation(value = "index测试")
     public String hello(String name){
         return ("hello m: "+name);
     }
