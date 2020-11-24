2ba68e0bac0f9bae0d66149a09b34f97d4873b7d
z <z>
2020-11-24 10:31:23 +0800

    swagger

--- server/src/main/java/com.zzq.kp.server/Application.java
+++ server/src/main/java/com.zzq.kp.server/Application.java
@@ -3,7 +3,6 @@ package com.zzq.kp.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.zzq.kp.biz","com.zzq.kp.server"})
